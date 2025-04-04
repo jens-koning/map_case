e01ab3635630de8f2435d44a530c5e4348fba191
Jens Koning <jens.jjk@gmail.com>
2025-04-04 11:27:01 +0200

    make simulation and update readme

--- simulate_utilization.py
+++ simulate_utilization.py
@@ -1,6 +1,10 @@
 #### Simulate utilization over 24hrs ####
+for v in dir(): del globals()[v]
+
+# load libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load the CSV data
 charger_info_df = pd.read_csv('charger_info.csv')
@@ -27,3 +31,20 @@ utilization_long_df = utilization_df.melt(id_vars=['hour'], var_name='charger_id
 
 # Save the simulated utilization data to CSV
 utilization_long_df.to_csv('charger_utilization.csv', index=False)
+
+# Plot utilization over time for a few chargers
+sample_chargers = utilization_long_df['charger_id'].unique()[:5]
+sample_data = utilization_long_df[utilization_long_df['charger_id'].isin(sample_chargers)]
+
+plt.figure(figsize=(12, 6))
+for charger_id in sample_chargers:
+    charger_data = sample_data[sample_data['charger_id'] == charger_id]
+    plt.plot(charger_data['hour'], charger_data['utilization'], label=f'Charger {charger_id}')
+
+plt.xlabel('Hour of the Day')
+plt.ylabel('Utilization')
+plt.title('Charger Utilization Over 24 Hours')
+plt.legend()
+plt.grid(True)
+plt.savefig('charger_utilization_over_time.png')
+plt.show()
